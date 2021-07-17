cfbb91be582284d4ed3375427a9c24587b5f8dd4
lars-tps <49183778+lars-tps@users.noreply.github.com>
2021-07-17 22:16:49 +0800

    testing messageManager.saveData()

--- src/Application.java
+++ src/Application.java
@@ -1,8 +1,14 @@
 import GUIComponents.AppFrame;
 
+import java.io.File;
+
 public class Application {
     public static void main(String[] args) {
-        new AppFrame();
-
+//        new AppFrame();
+//        MessageManager m = new MessageManager(".\\saves\\testCsv.csv");
+        MessageManager m = new MessageManager("."+ File.separator + "saves" + File.separator + "testCsv.csv");
+        m.addMessage(new Message(MoodLabel.GOOD, "I'm feeling good"));
+        m.addMessage(new Message(MoodLabel.GREAT, "I'm feeling great!"));
+        m.saveData();
     }
 }
