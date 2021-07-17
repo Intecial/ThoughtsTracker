2c10851b1b1b5eb5ac9f48d091ff49dde6582b6e
glee0021 <glee0021@student.monash.edu>
2021-07-17 17:00:52 +0900

    sorter

--- src/Sorter.java
+++ src/Sorter.java
@@ -1,24 +1,20 @@
-import java.util.Arrays;
-
-import static java.util.Collections.swap;
-
 public class Sorter {
 
-    public void quick_sort(int[] list){
+    public void quickSort(int[] list){
 
         int start = 0;
         int end = list.length -1;
-        quick_sort_aux(list, start, end);
+        quickSortAux(list, start, end);
 
     }
 
-    private void quick_sort_aux(int[] list, int start, int end) {
+    private void quickSortAux(int[] list, int start, int end) {
 
         if(start < end){
 
             int boundary = partition(list, start, end);
-            quick_sort_aux(list, start, boundary-1);
-            quick_sort_aux(list, boundary+1, end);
+            quickSortAux(list, start, boundary-1);
+            quickSortAux(list, boundary+1, end);
 
         }
 
@@ -53,20 +49,6 @@ public class Sorter {
         int temp = list[first];
         list[first] = list[second];
         list[second] = temp;
-
-    }
-
-    public static void main(String[] args) {
-
-        int list[] = {6,8,1,3,2,10};
-
-        System.out.println(Arrays.toString(list));
-
-        Sorter sort = new Sorter();
-
-        sort.quick_sort(list);
-
-        System.out.println(Arrays.toString(list));
 
     }
 
