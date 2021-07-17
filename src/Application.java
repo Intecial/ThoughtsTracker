c858c522026fdc0d8771fe3de5e622bf55c4199c
lars-tps <49183778+lars-tps@users.noreply.github.com>
2021-07-17 23:17:09 +0800

    wiped the test cases

--- src/Application.java
+++ src/Application.java
@@ -2,11 +2,5 @@ import MessagePackage.*;
 
 public class Application {
     public static void main(String[] args) {
-//        new AppFrame();
-//        MessageManager m = new MessageManager(".\\saves\\testCsv.csv");
-        MessageManager m = new MessageManager();
-        m.addMessage(new Message(MoodLabel.GOOD, "I'm feeling good"));
-        m.addMessage(new Message(MoodLabel.GREAT, "I'm feeling great!"));
-        m.saveData("testCsv.csv");
     }
 }
