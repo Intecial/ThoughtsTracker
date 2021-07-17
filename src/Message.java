5f26d3cda4ef7467a2a4b28d0db83ad266fafbbc
lars-tps <49183778+lars-tps@users.noreply.github.com>
2021-07-17 21:37:37 +0800

    New constructor to instantiate message from a string line. Updated to use LocalDateTime instead of GregorianCalendar.

--- src/Message.java
+++ src/Message.java
@@ -1,15 +1,38 @@
-import java.util.Calendar;
-import java.util.GregorianCalendar;
+import java.time.LocalDateTime;
 
 public class Message {
     private MoodLabel label;
     private String content;
-    private final Calendar timeDate;
+    private final LocalDateTime dateTime;
 
     public Message(MoodLabel label, String content) {
-        this.label = label;
-        this.content = content;
-        this.timeDate = new GregorianCalendar();
+        setLabel(label);
+        setContent(content);
+        this.dateTime = LocalDateTime.now();
+    }
+
+    public Message(String csvLine) {
+        String[] splitString = csvLine.split(",");
+        String strLabel = splitString[0];
+        String strContent = splitString[1];
+        String strDate = splitString[2];
+        String strTime = splitString[3];
+        // splitting date string
+        String[] splitDate = strDate.split("-");
+        int year = Integer.parseInt(splitDate[0]);
+        int month = Integer.parseInt(splitDate[1]);
+        int day = Integer.parseInt(splitDate[2]);
+        // splitting time string
+        String[] splitTime = strTime.split(":");
+        int hour = Integer.parseInt(splitTime[0]);
+        int minute = Integer.parseInt(splitTime[1]);
+        int seconds = Integer.parseInt(splitTime[2]);
+        int nano = Integer.parseInt(splitTime[3]);
+
+        setLabel(MoodLabel.valueOf(strLabel));
+        setContent(strContent);
+        this.dateTime = LocalDateTime.of(year, month, day, hour, minute, seconds, nano);
+
     }
 
     public String getContent() {
@@ -20,20 +43,24 @@ public class Message {
         return this.label;
     }
 
-    public Calendar getTimeDate() {
-        return timeDate;
+    public LocalDateTime getDateTime() {
+        return dateTime;
     }
 
-    public void changeContent(String content) {
+    public void setContent(String content) {
         this.content = content;
     }
 
-    public void changeLabel(MoodLabel label) {
+    public void setLabel(MoodLabel label) {
         this.label = label;
     }
 
     @Override
     public String toString() {
-        return  label + "," + content + "," + timeDate.getTime();
+        // GOOD,test message,2021-7-17,21:28:30:11
+        return  label + "," +
+                content + "," +
+                dateTime.getYear() + "-" + dateTime.getMonthValue() + "-" + dateTime.getDayOfMonth() + "," +
+                dateTime.getHour() + ":" + dateTime.getMinute() + ":" + dateTime.getSecond() + ":" + dateTime.getNano();
     }
 }
