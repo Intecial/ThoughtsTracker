033e8dbbec421878fac1dde0486b95eed63aad88
lars-tps <49183778+lars-tps@users.noreply.github.com>
2021-07-17 22:43:19 +0800

    updated test case for saveData

--- src/Application.java
+++ src/Application.java
@@ -6,9 +6,9 @@ public class Application {
     public static void main(String[] args) {
 //        new AppFrame();
 //        MessageManager m = new MessageManager(".\\saves\\testCsv.csv");
-        MessageManager m = new MessageManager("."+ File.separator + "saves" + File.separator + "testCsv.csv");
+        MessageManager m = new MessageManager();
         m.addMessage(new Message(MoodLabel.GOOD, "I'm feeling good"));
         m.addMessage(new Message(MoodLabel.GREAT, "I'm feeling great!"));
-        m.saveData();
+        m.saveData("testCsv.csv");
     }
 }
