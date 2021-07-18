50f50cdebaecebbe234537c5da44eb346485d2e4
lars-tps <49183778+lars-tps@users.noreply.github.com>
2021-07-18 21:48:42 +0800

    cleaning

--- src/Application.java
+++ src/Application.java
@@ -6,6 +6,5 @@ public class Application {
     public static void main(String[] args) {
         MessageManager m = new MessageManager();
         AppFrame frame = new AppFrame(m);
-        Statistics stats = new Statistics(m);
     }
 }
