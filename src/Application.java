9f6a01550cc4d7db8acc4a1317bfc7fe502f27e2
Intecial <ljuliano30@gmail.com>
2021-07-18 20:54:09 +0700

    Logo Added

--- src/Application.java
+++ src/Application.java
@@ -8,7 +8,6 @@ public class Application {
     public static void main(String[] args) {
         MessageManager m = new MessageManager();
         AppFrame frame = new AppFrame(m);
-        Statistics stats = new Statistics(m);
         ImageIcon icon = new ImageIcon("ThoughtsTracker_icon.png");
         frame.setIconImage(icon.getImage());
     }
