fefa974534e3da999f4cb31365a19693b576c226
lars-tps <49183778+lars-tps@users.noreply.github.com>
2021-07-18 16:25:09 +0800

    update

--- src/Application.java
+++ src/Application.java
@@ -3,12 +3,7 @@ import MessagePackage.*;
 
 public class Application {
     public static void main(String[] args) {
-//        new AppFrame();
-//        MessageManager m = new MessageManager(".\\saves\\testCsv.csv");
         MessageManager m = new MessageManager();
         AppFrame frame = new AppFrame(m);
-        m.addMessage(new Message(MoodLabel.GOOD, "I'm feeling good"));
-        m.addMessage(new Message(MoodLabel.GREAT, "I'm feeling great!"));
-        m.saveData("testCsv.csv");
     }
 }
