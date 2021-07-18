cccf192a839fe54fd32bdb8ade30370816811d0c
glee0021 <glee0021@student.monash.edu>
2021-07-18 17:32:14 +0900

    -

--- src/Statistics.java
+++ src/Statistics.java
@@ -5,13 +5,14 @@ import java.util.ArrayList;
 
 public class Statistics {
 
+    private MessageManager message;
+    private ArrayList<Message> mostUsed = new ArrayList<>();
+
     public Statistics(MessageManager message){
 
 
 
     }
-
-    private ArrayList<Message> mostUsed = new ArrayList<>();
 
     public void topFive(String fileName){
 
